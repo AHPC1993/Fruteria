ad1a4a97517d50882dcdc74181ff9af3c9f08383
Allan Pérez <allanh1193@outlook.com>
2016-11-08 10:03:45 -0500

    Proyecto finalizado con todas las funcionalidades pedidas

--- base de datos/db_fruteria (1).sql
+++ base de datos/db_fruteria (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 05-11-2016 a las 19:18:22
+-- Tiempo de generación: 08-11-2016 a las 10:03:07
 -- Versión del servidor: 5.5.50-0ubuntu0.14.04.1
 -- Versión de PHP: 5.5.9-1ubuntu4.17
 
@@ -49,19 +49,22 @@ INSERT INTO `tbl_clientes` (`cedula`, `nombres`, `apellidos`, `direccion`, `tele
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_productos` (
-  `id` int(12) NOT NULL,
+  `id` int(12) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) NOT NULL,
   `precio` int(10) NOT NULL,
-  `cantidad` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `cantidad` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Volcado de datos para la tabla `tbl_productos`
 --
 
 INSERT INTO `tbl_productos` (`id`, `nombre`, `precio`, `cantidad`) VALUES
-(1, 'Chocorramo', 1200, 70),
-(2, '2', 2, 2);
+(3, 'chocorramo', 1000, 10),
+(4, '2', 2, 2),
+(5, 'Bimbo', 0, 35),
+(6, 'Pan tajado', 0, 200);
 
 -- --------------------------------------------------------
 
@@ -83,7 +86,8 @@ CREATE TABLE IF NOT EXISTS `tbl_proveedores` (
 --
 
 INSERT INTO `tbl_proveedores` (`nit`, `nombre`, `direccion`, `telefono`, `producto`, `precio`) VALUES
-(1, '1', '1', '1', '1', 1);
+(1, '1', '1', '1', '1', 1),
+(890066066, 'Bimbo', 'Crr 11 ', '89191', 'Pan tajado', 2000);
 
 -- --------------------------------------------------------
 
@@ -107,9 +111,45 @@ CREATE TABLE IF NOT EXISTS `tbl_usuarios` (
 INSERT INTO `tbl_usuarios` (`cedula`, `nombres`, `apellidos`, `direccion`, `usuario`, `contrasena`) VALUES
 (123123, 'allan', 'perez', 'crr', 'allan', 'allan'),
 (1053, 'dniel', 'garcia', 'santos', 'dani', 'dani'),
+(123, '14', '14', '14', '14', '14'),
 (123, '21312312', '123123123', '3123123', '213123123', '123'),
 (45, 'DC', 'HM', 'BO', 'DC', 'DC'),
+(1, '1', '1', '1', '1', '1'),
+(12, '12', '1', '1', '1', '1'),
+(123, '12', '1', '1', '1', '1'),
+(1234, '12', '1', '1', '1', '1'),
+(12345, '12', '1', '1', '1', '1'),
+(123456, '12', '1', '1', '1', '1');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_ventas`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_ventas` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `producto` varchar(40) NOT NULL,
+  `cliente` varchar(40) NOT NULL,
+  `cantidad` int(3) NOT NULL,
+  `precio` int(8) NOT NULL,
+  `total` int(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Volcado de datos para la tabla `tbl_ventas`
+--
+
+INSERT INTO `tbl_ventas` (`id`, `producto`, `cliente`, `cantidad`, `precio`, `total`) VALUES
+(1, 'chocorramo', '12', 0, 0, 0),
+(2, 'chocorramo', '12', 0, 0, 0),
+(3, 'Pan tajado', '12', 10, 0, 0),
+(4, 'chocorramo', '12', 60, 0, 0),
+(5, 'chocorramo', '12', 5, 0, 0),
+(6, 'chocorramo', '12', 23, 3, 69),
+(7, 'chocorramo', '12', 22, 1000, 22000),
+(8, 'chocorramo', '12', 56, 1000, 56000);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
